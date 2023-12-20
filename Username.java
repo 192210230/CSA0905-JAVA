f1c14437fb622c97317355e47f11f35d8fd650e1
192210230 <131959949+192210230@users.noreply.github.com>
2023-12-20 12:59:20 +0530

    Add files via upload

---
+++ Username.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Username
+{
+ public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	String st,user=" ";
+	st="saveetha";
+	user=s.nextLine();
+	if(st.equals(user))
+	{  
+	 System.out.println("valid");
+        }
+ 	else
+	{
+	 System.out.println("not valid");
+	 }
+	}
+}
