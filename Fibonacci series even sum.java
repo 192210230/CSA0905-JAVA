ea748bca834f906181c18100a4eb0653f9f9c6f8
192210230 <131959949+192210230@users.noreply.github.com>
2023-12-20 13:31:05 +0530

    Fibonacci series even sum.java

---
+++ Fibonacci series even sum.java
@@ -0,0 +1,27 @@
+import java.util.*
+class Fibonacci even sum
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int n;
+		System.out.println("Enter n value");
+		n=s.nextInt();int n=input.nextInt();
+    int a1=0,a2=1,a3;
+    int a[]=new int[50];
+for(int i=0;i<10;i++)
+{
+ a[i]=a1;
+ System.out.print(a[i]+" ");
+ a3=a1+a2;
+ a1=a2;
+ a2=a3;
+}
+int sum=0;
+for(int i=0;i<=n*2;i=i+2)
+{
+ sum=sum+a[i];
+}
+System.out.println("\nSum: "+sum);
+  }
+}
